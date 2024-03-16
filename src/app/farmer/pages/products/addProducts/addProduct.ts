89652131792e22818dc4719cbd9091e8929c5738
Khomotso Dhlamini <139494185+KhomotsoDhlammini@users.noreply.github.com>
2024-03-17 01:13:31 +0200

    farmer can add products

--- src/app/farmer/pages/products/addProducts/addProduct.ts
+++ src/app/farmer/pages/products/addProducts/addProduct.ts
@@ -1,34 +1,65 @@
-import { Component } from "@angular/core";
-import { NavigateService } from "src/app/shared/services/navigate.service";
+import { Component, Input } from "@angular/core";
 import { UploadWidgetModule } from "@bytescale/upload-widget-angular";
-
 import { ModalOptions, initFlowbite } from 'flowbite';
 import { CommonModule } from "@angular/common";
 import { UserService } from "src/app/shared/services/user.service";
 import { Modal } from 'flowbite';
-
+import { UploadWidgetConfig, UploadWidgetResult } from "@bytescale/upload-widget";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { ProductService } from "src/app/shared/services/product.service";
+import { AlertService } from "src/app/shared/services/alert.service";
+import { NavigateService } from "src/app/shared/services/navigate.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'addNewProduct',
   templateUrl: './addNewProduct.html',
   styleUrls: ['./addNewProduct.scss'],
   standalone: true,
-  imports: [UploadWidgetModule, CommonModule],
+  imports: [UploadWidgetModule, ReactiveFormsModule, CommonModule],
 })
 
 export class AddProductPage {
-  /**
-   *
-   */
-  constructor(private navigate: NavigateService, public userService: UserService,) {
+  placeholderImage = "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png";
+  form = this.fb.nonNullable.group({
+    farmID: ['', Validators.required], // we need to get from login info
+    productName: ['', Validators.required],
+    productPrice: ['', Validators.required],
+    productImageURL: ['', Validators.required],
+    productDescription: ['', Validators.required]
+  })
+
+  farmOptions: UploadWidgetConfig = {
+    apiKey: 'free', // Get API keys from: www.bytescale.com
+    multi: false
+  };
+  productImageOnComplete = (files: UploadWidgetResult[]) => {
+    this.form.controls.productImageURL.setValue(files[0]?.fileUrl);
+  };
+
+  constructor(private fb: FormBuilder,
+    public userService: UserService,
+    private alert: AlertService,
+    public navigate: NavigateService,
+    private router: ActivatedRoute,
+    private productService: ProductService) {
   }
 
   ngOnInit(): void {
     initFlowbite();
   }
 
-
   onAddNewProductPopup() {
+    const modal = this.getModalRef();
+    modal.show();
+  }
+
+  closeModalPopup() {
+    const modal = this.getModalRef();
+    modal.hide();
+  }
+
+  getModalRef(): Modal {
     const $targetEl = document.getElementById('add-product-popup-modal');
 
     // options with default values
@@ -44,7 +75,30 @@ export class AddProductPage {
       id: 'add-product-popup-modal',
       override: true
     };
-    const modal = new Modal($targetEl, options, instanceOptions);
-    modal.show();
+    return new Modal($targetEl, options, instanceOptions);
+  }
+
+  onSubmit() {
+
+    this.form.controls.farmID.setValue(this.router.snapshot.paramMap.get('id') as string);
+    console.log(this.form.getRawValue())
+    if (this.form.valid) {
+      const formData = this.form.getRawValue();
+      this.productService.addProduct(formData).subscribe(
+        (response) => {
+          // Handle success, maybe show a success message or redirect to another page
+          this.alert.success('Succefully added a product');
+          this.closeModalPopup();
+          this.form.reset();
+           window.location.reload();
+          //this.navigate.to(`/farmer/farmdetails/${response.farmID}`)        
+        },
+        (error) => {
+          this.alert.error('Error adding product')
+        }
+      );
+    } else {
+      this.alert.error("Please input all details");
+    }
   }
 }
--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -1,6 +1,6 @@
 import {HttpClient} from '@angular/common/http'
 import {Injectable} from '@angular/core'
-import {Observable} from 'rxjs'
+import {Observable, map} from 'rxjs'
 import {ProductInterface} from '../types/product.interface'
 import {environment} from 'src/environments/environment.development'
 
@@ -21,4 +21,16 @@ export class ProductService {
     return this.http.delete<void>(`${SERVER}/products/${productID}`);
   }
   
+
+  addProduct(productData: any): Observable<any> {
+    const SERVER = environment.SERVER_URL;
+  
+    // Send a POST request to create a new farm
+    return this.http.post<any>(`${SERVER}/products/addProduct`, productData).pipe(
+      map(res => {
+        console.log('Response from server about adding a farm:', res);
+        return res;
+      })
+    );
+  }
 }
