00ae2e77d8d54845a64fb2fc600b4717601f0eac
Khomotso Dhlamini <139494185+KhomotsoDhlammini@users.noreply.github.com>
2024-03-16 20:42:29 +0200

    Can add new farm

--- src/app/farmer/pages/addNewFarm/addNewFarm.ts
+++ src/app/farmer/pages/addNewFarm/addNewFarm.ts
@@ -5,6 +5,8 @@ import { NavigateService } from "src/app/shared/services/navigate.service";
 import { UserService } from "src/app/shared/services/user.service";
 import { UploadWidgetModule } from "@bytescale/upload-widget-angular";
 import { UploadWidgetConfig, UploadWidgetResult } from "@bytescale/upload-widget";
+import { FarmService } from "src/app/shared/services/farm.service";
+import { AlertService } from "src/app/shared/services/alert.service";
 
 @Component({
   selector: 'farmer-addNewFarm',
@@ -23,13 +25,13 @@ export class AddNewFarm {
     farmLocation: ['', Validators.required],
     farmImageURL: ['', Validators.required],
     farmDescription: ['', Validators.required]
-  })  
-  
+  })
+
   farmOptions: UploadWidgetConfig = {
     apiKey: 'free', // Get API keys from: www.bytescale.com
     multi: false
   };
-  
+
   farmOnComplete = (files: UploadWidgetResult[]) => {
     this.form.controls.farmImageURL.setValue(files[0]?.fileUrl);
   };
@@ -37,13 +39,29 @@ export class AddNewFarm {
   /**
    *
    */
-  constructor(private fb: FormBuilder, public userService: UserService) {
+  constructor(private fb: FormBuilder, public userService: UserService,
+    private farmService: FarmService,
+    public navigate: NavigateService,
+    private alert: AlertService) {
   }
 
   onSubmit() {
-
-    console.log(this.form.getRawValue())
-    console.log(this.form.valid)
-
+    if (this.form.valid) {
+      const formData = this.form.getRawValue();
+      this.farmService.createFarm(formData).subscribe(
+        (response) => {
+          // Handle success, maybe show a success message or redirect to another page
+          console.log('Farm created successfully:', response);
+          this.alert.success('Succefully added a farm');
+          this.navigate.to(`/farmer/farmdetails/${response.farmID}`)        
+        },
+        (error) => {
+          this.alert.error('Error creating a farm')
+        }
+      );
+    } else {
+      this.alert.error("Please input all details");
+    }
   }
+
 }
--- src/app/shared/services/farm.service.ts
+++ src/app/shared/services/farm.service.ts
@@ -1,6 +1,6 @@
 import {HttpClient} from '@angular/common/http'
 import {Injectable} from '@angular/core'
-import {Observable} from 'rxjs'
+import {Observable, map} from 'rxjs'
 import {environment} from 'src/environments/environment.development'
 import {FarmInterface} from '../types/farm.interface'
 
@@ -30,5 +30,17 @@ export class FarmService {
     return this.http.get<FarmInterface[]>(
       SERVER + `/farm/farmerFarms/${farmerID}`
     )
+  }
+
+  createFarm(farmData: any): Observable<any> {
+    const SERVER = environment.SERVER_URL;
+  
+    // Send a POST request to create a new farm
+    return this.http.post<any>(`${SERVER}/farm/addNewFarm`, farmData).pipe(
+      map(res => {
+        console.log('Response from server about adding a farm:', res);
+        return res;
+      })
+    );
   }
 }
