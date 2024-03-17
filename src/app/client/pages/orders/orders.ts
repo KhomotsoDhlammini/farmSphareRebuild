1162d1d1a05e3d0fb8d36fec5018df4e4f8cbff0
Khomotso Dhlamini <139494185+KhomotsoDhlammini@users.noreply.github.com>
2024-03-17 20:28:17 +0200

    Orders showing

--- src/app/client/pages/orders/orders.ts
+++ src/app/client/pages/orders/orders.ts
@@ -1,6 +1,7 @@
 import { CommonModule, } from '@angular/common'
 import { Component, Input, OnInit } from '@angular/core'
 import { AlertService } from 'src/app/shared/services/alert.service';
+import { NavigateService } from 'src/app/shared/services/navigate.service';
 import { OrderService } from 'src/app/shared/services/order.service';
 import { UserService } from 'src/app/shared/services/user.service';
 import { OrderDetails } from 'src/app/shared/types/order.interface';
@@ -15,7 +16,7 @@ import { OrderDetails } from 'src/app/shared/types/order.interface';
 export class OrdersPage implements OnInit {
 
   ordersList: OrderDetails[] = [];
-  constructor(public orderService: OrderService, public userService: UserService, public alert: AlertService) {
+  constructor(public orderService: OrderService, public userService: UserService, public alert: AlertService, public navigateService: NavigateService) {
 
   }
 
